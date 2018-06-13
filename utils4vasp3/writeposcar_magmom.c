eeed295cd22104adb53da668555ec7d4edd7f1c7
Eunseok <cogito97@gmail.com>
2018-06-12 21:45:10 -0500

    Update and add files

--- utils4vasp3/writeposcar_magmom.c
+++ utils4vasp3/writeposcar_magmom.c
@@ -11,12 +11,14 @@
 
 int main(int argc, char *argv[]) {
     
-    FILE *fp, *fp1, *fp2, *fp3, *fp4;
+    FILE *fp, *fp1, *fp2, *fp3, *fp4, *fp5, *fp6;
     int i, np, howmanyLi, howmanyVa, howmanyMn, howmanyNi, howmanyCo;
     double *rp, *rpO, *kv;
     double tmp;
     char datfilename[100];
+    char magfilename[100];
     char poscarfilename[100];
+    char magmomfilename[100];
     char buff_line[200];
     
     fp = fopen("rp.dat","r");
@@ -107,8 +109,20 @@ int main(int argc, char *argv[]) {
     }
     fclose(fp4);
 
+    int mag_array[np];
+    sprintf(magfilename ,"%s",argv[2]);
+    printf("Reading the mag file, %s\n",magfilename);
+    fp5 = fopen(magfilename,"r");
+    for (i=0;i<np;i++) {
+        fscanf(fp5, "%d", &mag_array[i]);
+        printf("%d\n",mag_array[i]);
+    }
+    fclose(fp5);
+    
     sprintf(poscarfilename,"POSCAR");
+    sprintf(magmomfilename,"MAGMOM");
     fp = fopen(poscarfilename,"w");
+    fp6 = fopen(magmomfilename,"w");
     fprintf(fp,"LipNiqMnrCosO2\n");
     fprintf(fp,"1.0\n");
     fprintf(fp,"%.4f %.4f %.4f\n",*(kv+3*0),*(kv+3*0+1),*(kv+3*0+2));
@@ -137,26 +151,35 @@ int main(int argc, char *argv[]) {
 
     if (howmanyLi > 0)
         for (i=0;i<np;i++)
-            if (array[i]==sp_Li)
+            if (array[i]==sp_Li) {
                 fprintf(fp, "%.4f %.4f %.4f Li\n",*(rp+3*i),*(rp+3*i+1),*(rp+3*i+2));
+                fprintf(fp6,"%d\n", mag_array[i]);
+            }
     
     if (howmanyMn > 0)
         for (i=0;i<np;i++)
-            if (array[i]==sp_Mn)
+            if (array[i]==sp_Mn) {
                 fprintf(fp, "%.4f %.4f %.4f Mn\n",*(rp+3*i),*(rp+3*i+1),*(rp+3*i+2));
+                fprintf(fp6,"%d\n", mag_array[i]);
+            }
 
     if (howmanyNi > 0)
         for (i=0;i<np;i++)
-            if (array[i]==sp_Ni)
+            if (array[i]==sp_Ni) {
                 fprintf(fp, "%.4f %.4f %.4f Ni\n",*(rp+3*i),*(rp+3*i+1),*(rp+3*i+2));
+                fprintf(fp6,"%d\n", mag_array[i]);
+            }
     
     if (howmanyCo > 0)
         for (i=0;i<np;i++)
-            if (array[i]==sp_Co)
-                fprintf(fp, "%.4f %.4f %.4f Co\n",*(rp+3*i),*(rp+3*i+1),*(rp+3*i+2));
+            if (array[i]==sp_Co) {
+                fprintf(fp, "%.4f %.4f %.4f Ni\n",*(rp+3*i),*(rp+3*i+1),*(rp+3*i+2));
+                fprintf(fp6,"%d\n", mag_array[i]);
+            }
     
     for (i=0;i<np;i++)
         fprintf(fp, "%.4f %.4f %.4f O\n",*(rpO+3*i),*(rpO+3*i+1),*(rpO+3*i+2));
     
     fclose(fp);
+    fclose(fp6);
 }
